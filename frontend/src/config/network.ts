e865f0a1c63cd5927c9000eacae42f325a38f562
henessay <kolapivovarov4@gmail.com>
2026-04-01 03:02:12 +0200

    fix: use HTTPS RPC directly from VPS

--- frontend/src/config/network.ts
+++ frontend/src/config/network.ts
@@ -1,2 +1,2 @@
-export const RPC_URL = '/api/rpc'
+export const RPC_URL = 'https://204.168.233.1/rpc'
 export const SESSION_GAS_RESERVE = '0.1'
--- frontend/src/providers.tsx
+++ frontend/src/providers.tsx
@@ -13,7 +13,7 @@ import css from '@initia/interwovenkit-react/styles.css?inline'
 
 import { COSMOS_CHAIN_ID } from './config/chain'
 
-const RPC = '/api/rpc'
+const RPC = 'https://204.168.233.1/rpc'
 
 const initiaTestnet = {
   id: 2649570508581093,
